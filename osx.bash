1d9810fe3c384da862186d2f3420990e42e90c88
Akira Kusumoto <akirakusumo10@gmail.com>
2012-08-15 23:15:29 +0900

    macの設定を変更するbash追加

---
+++ osx.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Dockを自動的に隠す
+defaults write com.apple.dock autohide -bool true
+
+# Finder
+# 拡張子を常に表示する
+#defaults write -g AppleShowAllExtensions -bool true
+
+# .DS_Storeを作らない
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+# 拡張子変更時に確認を求めない
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool true
