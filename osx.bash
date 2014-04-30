ccceea056fece5434e2e3920a1010db502c5bb38
Akira Kusumoto <akirakusumo10@gmail.com>
2014-04-30 14:47:25 +0900

    Update osx.bash

--- osx.bash
+++ osx.bash
@@ -13,7 +13,7 @@ defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 # 拡張子変更時に確認を求めない
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool true
 
-# スクリーンショットの保存先を変更する
+# スクリーンショットの保存先を変更する(反映：$ killall SystemUIServer)
 defaults write com.apple.screencapture location ~/screenshot
 # Desktopにリンク作成
 ln -s ~/screenshot ~/Desktop/screenshot
