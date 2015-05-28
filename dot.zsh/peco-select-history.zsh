1bfdd74555b9615121cb6b1024b756469a634e74
Akira Kusumoto <akirakusumo10@gmail.com>
2015-05-28 13:39:38 +0900

    重複を排除した

--- dot.zsh/peco-select-history.zsh
+++ dot.zsh/peco-select-history.zsh
@@ -1,5 +1,6 @@
 # http://blog.kenjiskywalker.org/blog/2014/06/12/peco/
 # http://qiita.com/uchiko/items/f6b1528d7362c9310da0
+# http://qiita.com/wada811/items/78b14181a4de0fd5b497
 
 function peco-select-history() {
     local tac
@@ -8,9 +9,7 @@ function peco-select-history() {
     else
         tac="tail -r"
     fi
-    BUFFER=$(\history -n 1 | \
-        eval $tac | \
-        peco --query "$LBUFFER")
+    BUFFER=$(history -n 1 | eval $tac | awk '!a[$0]++' | peco --query "$LBUFFER")
     CURSOR=$#BUFFER
     zle clear-screen
 }
